f4aa82686ce6f18ea9083967d4b70a6e5d1eb09c
Beatriz dos Santos Vieira <110930799+beatrizsantosvieira@users.noreply.github.com>
2024-05-30 18:02:58 -0300

    Update Principal.java

--- ap2_ex3/Principal.java
+++ ap2_ex3/Principal.java
@@ -2,16 +2,16 @@ package ap2_ex3;
 
 public class Principal {
 	 public static void main(String[] args) {
-		// Cria uma instância de Colaborador
+		 // Cria uma instância de Colaborador
 		 Colaborador colaborador = new Colaborador("Beatriz Vieira", 22, 200);
 		 
 		// Cria uma instância de CalculaBonus
-	     CalculaBonus calculaBonus = new CalculaBonus();
-	     System.out.println("Bonus: " + calculaBonus.calculaBonus(colaborador));
+	     	CalculaBonus calculaBonus = new CalculaBonus();
+	     	System.out.println("Bonus: " + calculaBonus.calculaBonus(colaborador));
 
-	     // Cria uma instância de SalvaDados
-	     SalvaDados salvaDados = new SalvaDados();
-	     // Salva os dados do colaborador
-	     salvaDados.save(colaborador);
+	     	// Cria uma instância de SalvaDados
+	     	SalvaDados salvaDados = new SalvaDados();
+	     	// Salva os dados do colaborador
+	     	salvaDados.save(colaborador);
 	 }
 }
