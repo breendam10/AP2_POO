0e805e38d0435f10d0f112369b26333fee75f090
Beatriz dos Santos Vieira <110930799+beatrizsantosvieira@users.noreply.github.com>
2024-05-30 18:22:46 -0300

    Delete ap2_ex3 directory

--- ap2_ex3/SalvaDados.java
+++
@@ -1,9 +0,0 @@
-package ap2_ex3;
-
-// Classe responsável EXCLUSIVAMENTE por salvar os dados do empregado:
-class SalvaDados {
-	public void save(Colaborador colaborador) {
-		// Código para salvar os dados do empregado no banco de dados
-		System.out.println("Salvando os dados do colaborador para " + colaborador.getNome());
-	}
-}
